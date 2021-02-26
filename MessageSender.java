a0f3bfba4d87a843579d541562f745064b287528
Conor Finn <cf338@kent.ac.uk>
2021-02-26 17:58:10 +0000

    Implement basic functionality
    Still needs checksum and message splitting. Good enough for testing I
    suppose

--- MessageSender.java
+++ MessageSender.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -7,6 +8,13 @@ public class MessageSender
 {
     // maximum transfer unit (frame length limit)
     private final int mtu;
+
+    // Minimum frame length
+    private final int META_LEN = "[F~00~~00]".length();
+
+    // Maximum MTU
+    private final int MAX_MTU = 109;
+
     // Source of the messages.
     private final Scanner stdin;
 
@@ -17,7 +25,7 @@ public class MessageSender
      * not exceed the MTU)
      */
     public MessageSender(int mtu) {
-        this.mtu = mtu;
+        this.mtu = Math.min(mtu, MAX_MTU);
         this.stdin = new Scanner(System.in);
     }
 
@@ -29,13 +37,45 @@ public class MessageSender
     public void sendMessage() {
         String message = stdin.nextLine();
         if(message != null) {
-            // This part needs completing.
-            System.out.println(message);
+            ArrayList<String> segments;
+            if (message.length() + META_LEN > mtu) {
+                // TODO: If long, split into segments
+                segments = getSegments(message);
+            } else {
+                // Otherwise just need the one segment
+                segments = new ArrayList<>();
+                segments.add(message);
+            }
+            for (int i = 0 ; i < segments.size() ; i++) {
+                // Determine frame type (D for data, F for final)
+                String frameType = i+1 == segments.size() ? "F" : "D";
+                // Add basic frame info
+                String frame = "[" + frameType + "~";
+                // Add frame length
+                if (segments.get(i).length() > 10) {
+                    frame += segments.get(i).length();
+                } else {
+                    frame += "0" + segments.get(i).length();
+                }
+                // Add message
+                frame += "~" + segments.get(i) + "~";
+                // TODO: Generate checksum
+                frame += "00";
+                // Add final frame delimiter
+                frame += "]";
+                System.out.println(frame);
+            }
         }
         else {
             System.err.println("No message received.");
         }
 
+    }
+
+    private ArrayList<String> getSegments(String message) {
+        ArrayList<String> segments = new ArrayList<>();
+
+        return segments;
     }
 }
 
